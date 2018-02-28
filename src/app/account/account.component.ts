1bc0ee294abb95217df49537b68771dd2a194277
Chris Nielson <ctniel1@gmail.com>
2018-02-27 17:59:28 -0700

    link sets started, account page started

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -11,6 +11,8 @@ import {LinkService} from '../link.service';
 })
 export class AccountComponent implements OnInit {
 
+  accountImage = 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/16406891_10211216429622181_5318879557385017600_n.jpg?oh=cf53c813743fef217b9dd70c6051c5b1&oe=5B09975B';
+
   constructor(private router: Router, private linkService: LinkService) { }
 
   ngOnInit() {
--- src/app/set/set.component.ts
+++ src/app/set/set.component.ts
@@ -13,6 +13,18 @@ import { LinkService } from '../link.service';
 export class SetComponent implements OnInit {
 
   links: Link[] = [];
+  mobileSettings: boolean = true;
+  titles: boolean = true;
+  linkAdd: boolean = false;
+  linkSet: boolean = false;
+  linkManage: boolean = false;
+  BYUSets: boolean = true;
+  OtherSets: boolean = false;
+  defaultIconList: boolean = false;
+  selectedIcon: string = "";
+  selectedTitle: string = "";
+  selectedCategory: string = "";
+  selectedURL: string = "";
 
   constructor(private router: Router, private linkService: LinkService) { }
 
@@ -23,6 +35,130 @@ export class SetComponent implements OnInit {
   getLinks(): void {
     this.linkService.getLinks()
       .subscribe(links => this.links = links);
+  }
+
+  addLink() {
+    this.linkAdd = true;
+    if (this.linkSet == true) {
+      this.linkSet = false;
+    }
+    if (this.linkManage == true) {
+      this.linkManage = false;
+    }
+    if (this.defaultIconList == true) {
+      this.defaultIconList = false;
+    }
+  }
+
+  addSet() {
+    this.linkSet = true;
+    if (this.linkAdd == true) {
+      this.linkAdd = false;
+    }
+    if (this.linkManage == true) {
+      this.linkManage = false;
+    }
+    if (this.defaultIconList == true) {
+      this.defaultIconList = false;
+    }
+  }
+
+  addLinkMobile() {
+    this.mobileSettings = false;
+    this.linkAdd = true;
+  }
+
+  addSetMobile() {
+    this.mobileSettings = false;
+    this.linkSet = true;
+  }
+
+  addedLink() {
+    this.linkAdd = false;
+  }
+
+  addedSet() {
+    this.linkSet = false;
+  }
+
+  addedSetMobile() {
+    this.linkSet = false;
+    this.mobileSettings = true;
+  }
+
+  addedLinkMobile() {
+    this.linkAdd = false;
+    this.mobileSettings = true;
+  }
+
+  manageLink() {
+    this.linkManage = true;
+    if (this.linkAdd == true){
+      this.linkAdd = false;
+    }
+    if (this.linkSet == true) {
+      this.linkSet = false;
+    }
+    if (this.defaultIconList == true) {
+      this.defaultIconList = false;
+    }
+  }
+
+  manageLinkMobile() {
+    this.mobileSettings = false;
+    this.linkManage = true;
+  }
+
+  managedLink() {
+    this.linkManage = false;
+  }
+
+  managedLinkMobile() {
+    this.linkManage = false;
+    this.mobileSettings = true;
+  }
+
+  toggleBYUSets() {
+    if (this.BYUSets == false) {
+      this.BYUSets = true;
+    }
+    if (this.OtherSets == true) {
+      this.OtherSets = false;
+    }
+  }
+
+  toggleOtherSets() {
+    if (this.OtherSets == false) {
+      this.OtherSets = true;
+    }
+    if (this.BYUSets == true) {
+      this.BYUSets = false;
+    }
+  }
+
+  toggleDefaultIcons() {
+    if (this.linkAdd == true) {
+      this.linkAdd = false;
+    }
+    this.defaultIconList = true;
+  }
+
+  selectIcon(icon) {
+    this.selectedIcon = icon;
+    this.defaultIconList = false;
+    this.linkAdd = true;
+  }
+
+  submitLink() {
+    console.log(this.selectedCategory + ", " + this.selectedTitle + ", " + this. selectedIcon + ", " + this.selectedURL);
+  }
+
+  toggleIcons() {
+
+  }
+
+  toggleTitles() {
+
   }
 
 }
